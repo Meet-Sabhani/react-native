d2dbe0185b860793b1209c7d24e45720d7397a1b
Meet#2605 <meetsabhani1@gmail.com>
2024-04-15 23:03:34 +0530

    first commit

---
+++ home/App.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import { View, Text, StyleSheet } from 'react-native';
+
+const App = () => {
+  return (
+    <View style={styles.container}>
+      <Text>Hello, meet How r you ?!</Text>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor:"blue",
+  },
+});
+
+export default App;
